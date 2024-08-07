2f065c202b300cafbca092b26fb9477aefe8d31c
Aayush Verma <124078703+Aayushv007@users.noreply.github.com>
2024-08-08 02:53:56 +0530

    Add files via upload

--- RX/uart_rx.v
+++ RX/uart_rx.v
@@ -1,20 +1,4 @@
-// AstroTinker Bot : Task 2A : UART Receiver
-/*
-Instructions
--------------------
-Students are not allowed to make any changes in the Module declaration.
-
-This file is used to receive UART Rx data packet from receiver line and then update the rx_msg and rx_complete data lines.
-
-Recommended Quartus Version : 20.1
-The submitted project file must be 20.1 compatible as the evaluation will be done on Quartus Prime Lite 20.1.
-
-Warning: The error due to compatibility will not be entertained.
--------------------
-*/
-
-/*
-Module UART Receiver
+/*Module UART Receiver
 
 Input:  clk_50M - 50 MHz clock
         rx      - UART Receiver
@@ -29,10 +13,6 @@ module uart_rx (
   output reg [7:0] rx_msg,
   output reg rx_complete
 );
-
-//////////////////DO NOT MAKE ANY CHANGES ABOVE THIS LINE//////////////////
-
-////////////////////////// Add your code here
 
 
 initial begin
@@ -83,6 +63,5 @@ always @(posedge clk_50M) begin
 	end
    counter = counter + 1;
 end
-//////////////////DO NOT MAKE ANY CHANGES BELOW THIS LINE//////////////////
 
 endmodule
